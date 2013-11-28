3d2ad333d8e08d23e402566c54aeb820bab6ac96
Blechd0se <alex.christ@hotmail.de>
2013-11-28 16:50:54 +0100

    tegra: add smarter hotplug routine
    This hotplug driver for cpus is based upon
    @franciscofranco awesome work for various nexus
    devices.
    
    While i liked the idea to bound cpu0 and cpu2
    as well as cpu1 and cpu3 together, i wanted to
    let the hotplug driver be more on some sort of
    high performance mode. In general my additions
    should online 4 cores more often than the orginal
    implementation and it should stay at this level
    for some time until it recalculates the load.
    
    Next (planned) things;
     * consider hotplug only for lowest thread intensive core
     * add thermal routines
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

---
+++ arch/arm/kernel/tegra_hotplug.c
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2013, Francisco Franco <franciscofranco.1990@gmail.com>. 
+ *
+ * Small algorithm changes for more performance
+ * Copyright (c) 2013, Alexander Christ <alex.christ@hotmail.de>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Simple no bullshit hot[un]plug driver for SMP
+ */
+
+/*
+ * TODO   - Hotplug driver makes static decisions (thread migraten could be expansive)
+ *        - Find lowest working core for shutdown
+ *	  - Add Thermal Throttle Driver
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/cpu.h>
+#include <linux/workqueue.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/earlysuspend.h>
+#include <linux/cpufreq.h>
+#include <linux/delay.h>
+#include <linux/hotplug.h>
+ 
+#define DEFAULT_FIRST_LEVEL	75
+#define LOAD_BALANCER		30
+#define HIGH_LOAD_COUNTER	20
+#define SAMPLING_RATE_MS	500
+
+struct cpu_stats
+{
+	unsigned int total_cpus;
+	unsigned int default_first_level;
+	unsigned int suspend_frequency;
+
+	/* For the three hot-plug-able Cores */
+	unsigned int counter[2];
+};
+
+struct cpu_load_data {
+        u64 prev_cpu_idle;
+        u64 prev_cpu_wall;
+};
+
+static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
+
+
+static struct cpu_stats stats;
+static struct workqueue_struct *wq;
+static struct delayed_work decide_hotplug;
+
+static unsigned long down_delay;
+
+
+static inline int get_cpu_load(unsigned int cpu)
+{
+	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+	struct cpufreq_policy policy;
+	u64 cur_wall_time, cur_idle_time;
+	unsigned int idle_time, wall_time;
+	unsigned int cur_load;
+
+	cpufreq_get_policy(&policy, cpu);
+
+	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, true);
+
+	wall_time = (unsigned int) (cur_wall_time - pcpu->prev_cpu_wall);
+	pcpu->prev_cpu_wall = cur_wall_time;
+
+	idle_time = (unsigned int) (cur_idle_time - pcpu->prev_cpu_idle);
+	pcpu->prev_cpu_idle = cur_idle_time;
+
+	if (unlikely(!wall_time || wall_time < idle_time))
+		return 0;
+
+	cur_load = 100 * (wall_time - idle_time) / wall_time;
+
+	return (cur_load * policy.cur) / policy.max;
+}
+
+/*
+ * Calculates the load for a given cpu
+ */ 
+
+static void calculate_load_for_cpu(int cpu) 
+{
+
+	for_each_online_cpu(cpu) 
+	{
+		/*  
+		 * We are above our threshold, so update our counter for cpu;
+		 */
+		if (get_cpu_load(cpu) >= stats.default_first_level
+			&& likely(stats.counter[cpu] < HIGH_LOAD_COUNTER)) {
+			stats.counter[cpu] += 2;
+		}
+
+		else {
+			if (stats.counter[cpu] > 0)
+				stats.counter[cpu]--;
+		}
+
+		/* Reset CPU */
+		if (cpu) {
+			cpu = 0;
+			break;
+		}
+	}	
+
+}
+
+/**
+ * Simple load based decision algorithm to determ
+ * how many cores should be on- or offlined
+ */
+
+static void decide_hotplug_func(struct work_struct *work)
+{
+	unsigned long now = jiffies;
+	static unsigned long last_change_time;
+	int i, j;
+
+	/* Do load calculation for each cpu counter */
+
+	for (i = 0, j = 2; i < 2; i++, j++) {
+		calculate_load_for_cpu(i);
+
+		if (stats.counter[i] >= 10) {
+			if (!cpu_online(j)) {
+				printk("[Hot-Plug]: CPU%u ready for onlining\n", j);
+				cpu_up(j);
+			}
+		}
+		else {
+			/* Calculate again */
+			if ((now + SAMPLING_RATE_MS) > last_change_time) {
+				calculate_load_for_cpu(i);
+
+				if (stats.counter[i] > 0 && cpu_online(j)) {
+					printk("[Hot-Plug]: CPU%u ready for offlining\n", j);
+						cpu_down(j);
+						last_change_time = now;
+				}
+			}
+		}
+	}
+	
+	/* Make a dedicated work_queue */
+	queue_delayed_work(wq, &decide_hotplug, down_delay);
+}
+
+static void grouper_hotplug_early_suspend(struct early_suspend *handler)
+{	 
+	int cpu;
+
+	/* cancel the hotplug work when the screen is off and flush the WQ */
+	flush_workqueue(wq);
+	cancel_delayed_work_sync(&decide_hotplug);
+
+	pr_info("Early Suspend stopping Hotplug work...\n");
+    
+	for_each_online_cpu(cpu) 
+	{
+		if (cpu) {
+			cpu_down(cpu);
+		}
+	}
+}
+
+static void grouper_hotplug_late_resume(struct early_suspend *handler)
+{  
+	int cpu;
+
+	/* online all cores when the screen goes online */
+	for_each_possible_cpu(cpu) 
+		if (cpu)
+			cpu_up(cpu);
+
+	/* Resetting Counters */
+	stats.counter[0] = 0;
+	stats.counter[1] = 0;
+
+
+	pr_info("Late Resume starting Hotplug work...\n");
+	queue_delayed_work(wq, &decide_hotplug, HZ);
+}
+
+static struct early_suspend grouper_hotplug_suspend =
+{
+	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1,
+	.suspend = grouper_hotplug_early_suspend,
+	.resume = grouper_hotplug_late_resume,
+};
+
+int __init grouper_hotplug_init(void)
+{
+	pr_info("Grouper Hotplug driver started.\n");
+    
+	/* init everything here */
+	stats.total_cpus = num_present_cpus();
+	stats.default_first_level = DEFAULT_FIRST_LEVEL;
+	down_delay = msecs_to_jiffies(SAMPLING_RATE_MS);
+	
+	/* Resetting Counters */
+	stats.counter[0] = 0;
+	stats.counter[1] = 0;
+
+	wq = alloc_ordered_workqueue("grouper_hotplug_workqueue", 0);
+    
+	if (!wq)
+		return -ENOMEM;
+    
+	INIT_DELAYED_WORK(&decide_hotplug, decide_hotplug_func);
+	queue_delayed_work(wq, &decide_hotplug, down_delay);
+    
+	register_early_suspend(&grouper_hotplug_suspend);
+    
+	return 0;
+}
+late_initcall(grouper_hotplug_init);
+
+
---
+++ include/linux/hotplug.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013, Francisco Franco <franciscofranco.1990@gmail.com>.
+ *
+ * Copyright (c) 2013, Alexander Christ <alex.christ@hotmail.de>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_HOTPLUG_H
+#define _LINUX_HOTPLUG_H
+
+extern unsigned int report_load_at_max_freq(int cpu);
+
+#endif
+
